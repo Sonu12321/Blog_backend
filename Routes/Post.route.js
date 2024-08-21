7bd985bbf874f43f0001f803a5a5144d5e30742a
Sonu Santhosh <sonusanthosh371@gmail.com>
2024-08-22 01:11:02 +0530

    update

--- Routes/Post.route.js
+++ Routes/Post.route.js
@@ -1,11 +1,11 @@
 import express from 'express'
 import { verifyToken } from '../Utils/Verifyuser.js'
-import { create, deleteposts, getposts } from '../controllers/Post.controller.js'
+import { create, deleteposts, getposts,updatepost } from '../controllers/Post.controller.js'
 
 const router = express.Router()
 
 router.post('/create',verifyToken,create)
 router.get('/getposts',getposts)
 router.delete('/deleteposts/:postId/:userId',verifyToken,deleteposts)
-
+router.put('/updatepost/:postId/:userId', verifyToken, updatepost)
 export default router
