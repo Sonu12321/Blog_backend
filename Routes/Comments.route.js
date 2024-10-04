4b06b00590ebcf023b47a7aa0ef84a4b6b4a4219
Sonu Santhosh <sonusanthosh371@gmail.com>
2024-10-04 18:26:49 +0530

    comments

--- Routes/Comments.route.js
+++ Routes/Comments.route.js
@@ -1,6 +1,6 @@
 import express from 'express'
 // import createComment from '../controllers/comment.controller.js'
-import { createComment, getPostComments } from '../controllers/comment.controller.js'
+import { createComment, deleteComment, editComment, getcomments, getPostComments, likeComment } from '../controllers/comment.controller.js'
 import { verifyToken } from '../Utils/Verifyuser.js'
 
 
@@ -8,4 +8,9 @@ const router = express.Router()
 
 router.post('/create', verifyToken, createComment);
 router.get('/getPostComments/:postId', getPostComments);
+router.put('/likeComment/:commentId', verifyToken, likeComment);
+router.put('/editComment/:commentId', verifyToken, editComment);
+router.delete('/deleteComment/:commentId', verifyToken, deleteComment);
+router.get('/getcomments', verifyToken, getcomments);
+
 export default router;
